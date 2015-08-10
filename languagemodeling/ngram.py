4f0924f396cdfc6d60a84cf961ab97c5ba42a868
Franco M. Luque <francolq@famaf.unc.edu.ar>
2015-08-10 18:15:01 +0200

    Implement n-gram model. Basic tests.

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -3,19 +3,31 @@ from collections import defaultdict
 
 
 class NGram(object):
-    
+
     def __init__(self, n, sents):
         """
         n -- order of the model.
         sents -- list of sentences, each one being a list of tokens.
         """
-
+        assert n > 0
+        self.n = n
         self.counts = counts = defaultdict(int)
 
         for sent in sents:
             for i in range(len(sent) - n + 1):
-                if n == 1:
-                    ngram = sent[i]
-                else:
-                    ngram = tuple(sent[i: i + n])
+                ngram = tuple(sent[i: i + n])
                 counts[ngram] += 1
+                counts[ngram[:-1]] += 1
+
+    def prob(self, token, prev_tokens=None):
+        n = self.n
+        if not prev_tokens:
+            prev_tokens = []
+        assert len(prev_tokens) == n - 1
+
+        tokens = prev_tokens + [token]
+        return float(self.counts[tuple(tokens)]) / self.counts[tuple(prev_tokens)]
+
+
+class SmoothNGram:
+    pass
--- languagemodeling/tests/test_ngrams.py
+++ languagemodeling/tests/test_ngrams.py
@@ -6,38 +6,41 @@ from languagemodeling.ngram import NGram
 
 class TestNGram(TestCase):
 
-    def test_init_1gram(self):
-
-        sents = [
+    def setUp(self):
+        self.sents = [
             'el gato come pescado .'.split(),
             'la gata come salmón .'.split(),
         ]
 
-        ngram = NGram(1, sents)
+    def test_init_1gram(self):
+        ngram = NGram(1, self.sents)
 
         counts = {
-            'el': 1,
-            'gato': 1,
-            'come': 2,
-            'pescado': 1,
-            '.': 2,
-            'la': 1,
-            'gata': 1,
-            'salmón': 1,
+            (): 10,
+            ('el',): 1,
+            ('gato',): 1,
+            ('come',): 2,
+            ('pescado',): 1,
+            ('.',): 2,
+            ('la',): 1,
+            ('gata',): 1,
+            ('salmón',): 1,
         }
 
         self.assertEqual(dict(ngram.counts), counts)
 
     def test_init_2gram(self):
-
-        sents = [
-            'el gato come pescado .'.split(),
-            'la gata come salmón .'.split(),
-        ]
-
-        ngram = NGram(2, sents)
+        ngram = NGram(2, self.sents)
 
         counts = {
+            ('el',): 1,
+            ('gato',): 1,
+            ('come',): 2,
+            ('pescado',): 1,
+            # ('.',): 2,
+            ('la',): 1,
+            ('gata',): 1,
+            ('salmón',): 1,
             ('el', 'gato'): 1,
             ('gato', 'come'): 1,
             ('come', 'pescado'): 1,
@@ -49,3 +52,15 @@ class TestNGram(TestCase):
         }
 
         self.assertEqual(dict(ngram.counts), counts)
+
+    def test_prob_1gram(self):
+        ngram = NGram(1, self.sents)
+
+        self.assertEqual(ngram.prob('pescado'), 0.1)
+        self.assertEqual(ngram.prob('come'), 0.2)
+
+    def test_prob(self):
+        ngram = NGram(2, self.sents)
+
+        self.assertEqual(ngram.prob('pescado', ['come']), 0.5)
+        self.assertEqual(ngram.prob('salmón', ['come']), 0.5)

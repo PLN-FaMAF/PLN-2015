544f920c2170cc67f20c06aed1e7ece255efb499
Franco M. Luque <francolq@famaf.unc.edu.ar>
2015-10-06 16:41:57 -0300

    Ancora corpus reader: - some docs and pep8. - nicer code. - Fix tagged sents to avoid returning None words. - Now lazy iterators using LazyConcatenation. Repeated iteration is not supported yet.

--- corpus/ancora.py
+++ corpus/ancora.py
@@ -1,11 +1,16 @@
 from nltk.corpus.reader.api import SyntaxCorpusReader
 from nltk.corpus.reader import xmldocs
 from nltk import tree
-from nltk.util import LazyMap
+from nltk.util import LazyMap, LazyConcatenation
 from nltk.corpus.reader.util import concat
 
 
 def parsed(element):
+    """Converts a 'sentence' XML element (xml.etree.ElementTree.Element) to
+    an NLTK tree.
+
+    element -- the XML sentence element (or a subelement)
+    """
     if element:
         # element viewed as a list is non-empty (it has subelements)
         subtrees = map(parsed, element)
@@ -16,21 +21,41 @@ def parsed(element):
         if element.get('elliptic') == 'yes':
             return None
         else:
-            return tree.Tree(element.get('pos') or element.get('ne') or 'unk', [element.get('wd')])
+            return tree.Tree(element.get('pos') or element.get('ne') or 'unk',
+                             [element.get('wd')])
 
 
 def tagged(element):
+    """Converts a 'sentence' XML element (xml.etree.ElementTree.Element) to
+    a tagged sentence.
+
+    element -- the XML sentence element (or a subelement)
+    """
     # http://www.w3schools.com/xpath/xpath_syntax.asp
     # XXX: XPath '//*[@wd]' not working
-    #return [(x.get('wd'), x.get('pos') or x.get('ne')) for x in element.findall('*//*[@wd]')] + [('.', 'fp')]
-    return list(filter(lambda x: x != (None, None), parsed(element).pos()))
+    # return [(x.get('wd'), x.get('pos') or x.get('ne'))
+    #         for x in element.findall('*//*[@wd]')] + [('.', 'fp')]
+
+    # convert to tree and get the tagged sent
+    pos = parsed(element).pos()
+    # filter None words (may return an emtpy list)
+    return list(filter(lambda x: x[0] is not None, pos))
 
 
 def untagged(element):
+    """Converts a 'sentence' XML element (xml.etree.ElementTree.Element) to
+    a sentence.
+
+    element -- the XML sentence element (or a subelement)
+    """
     # http://www.w3schools.com/xpath/xpath_syntax.asp
     # XXX: XPath '//*[@wd]' not working
-    #return [x.get('wd') for x in element.findall('*//*[@wd]')] + [('.', 'fp')]
-    return list(filter(lambda x: x is not None, parsed(element).leaves()))
+    # return [x.get('wd') for x in element.findall('*//*[@wd]')] + [('.', 'fp')]
+
+    # convert to tree and get the sent
+    sent = parsed(element).leaves()
+    # filter None words (may return an emtpy list)
+    return list(filter(lambda x: x is not None, sent))
 
 
 class AncoraCorpusReader(SyntaxCorpusReader):
@@ -39,24 +64,25 @@ class AncoraCorpusReader(SyntaxCorpusReader):
         if files is None:
             files = '.*\.tbf\.xml'
         self.xmlreader = xmldocs.XMLCorpusReader(path, files)
-    
+
     def parsed_sents(self, fileids=None):
-        if not fileids:
-            fileids = self.xmlreader.fileids()
-        return LazyMap(parsed, concat([list(self.xmlreader.xml(fileid)) for fileid in fileids]))
+        return LazyMap(parsed, self.elements(fileids))
 
     def tagged_sents(self, fileids=None):
-        if not fileids:
-            fileids = self.xmlreader.fileids()
-        return LazyMap(tagged, concat([list(self.xmlreader.xml(fileid)) for fileid in fileids]))
+        return LazyMap(tagged, self.elements(fileids))
 
     def sents(self, fileids=None):
-        # FIXME: not lazy!
+        return LazyMap(untagged, self.elements(fileids))
+
+    def elements(self, fileids=None):
+        # FIXME: skip sentence elements that will result in empty sentences!
         if not fileids:
             fileids = self.xmlreader.fileids()
-        return LazyMap(untagged, concat([list(self.xmlreader.xml(fileid)) for fileid in fileids]))
+        # xml() returns a top element that is also a list of sentence elements
+        return LazyConcatenation(self.xmlreader.xml(f) for f in fileids)
 
     def tagged_words(self, fileids=None):
+        # XXX: use LazyConcatenation?
         return concat(self.tagged_sents(fileids))
 
     def __repr__(self):
@@ -71,4 +97,5 @@ class SimpleAncoraCorpusReader(AncoraCorpusReader):
         super().__init__(path, files)
 
     def tagged_sents(self, fileids=None):
-        return LazyMap(lambda s: [(w, t[:2]) for w, t in s], super().tagged_sents(fileids))
+        f = lambda s: [(w, t[:2]) for w, t in s]
+        return LazyMap(f, super().tagged_sents(fileids))

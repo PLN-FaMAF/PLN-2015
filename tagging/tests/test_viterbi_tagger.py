7f68c57e7b5d0cbc4e23690f72a732b0333d1b9f
Franco M. Luque <francolq@famaf.unc.edu.ar>
2015-10-14 12:26:45 -0300

    More interesting tests for HMM and ViterbiTagger with ambiguity.

--- tagging/tests/test_viterbi_tagger.py
+++ tagging/tests/test_viterbi_tagger.py
@@ -44,8 +44,53 @@ class TestViterbiTagger(TestCase):
 
         self.assertEqual(y, 'D N V'.split())
 
+    def test_tag2(self):
+        tagset = {'D', 'N', 'V'}
+        trans = {
+            ('<s>', '<s>'): {'D': 1.0},
+            ('<s>', 'D'): {'N': 1.0},
+            ('D', 'N'): {'V': 0.8, 'N': 0.2},
+            ('N', 'N'): {'V': 1.0},
+            ('N', 'V'): {'</s>': 1.0},
+        }
+        out = {
+            'D': {'the': 1.0},
+            'N': {'dog': 0.4, 'barks': 0.6},
+            'V': {'dog': 0.1, 'barks': 0.9},
+        }
+        hmm = HMM(3, tagset, trans, out)
+        tagger = ViterbiTagger(hmm)
+
+        x = 'the dog barks'.split()
+        y = tagger.tag(x)
+
+        pi = {
+            0: {
+                ('<s>', '<s>'): (log2(1.0), []),
+            },
+            1: {
+                ('<s>', 'D'): (log2(1.0), ['D']),
+            },
+            2: {
+                ('D', 'N'): (log2(0.4), ['D', 'N']),
+            },
+            3: {
+                ('N', 'V'): (log2(0.8 * 0.4 * 0.9), ['D', 'N', 'V']),
+                ('N', 'N'): (log2(0.2 * 0.4 * 0.6), ['D', 'N', 'N']),
+            }
+        }
+        self.assertEqualPi(tagger._pi, pi)
+
+        self.assertEqual(y, 'D N V'.split())
+
     def assertEqualPi(self, pi1, pi2):
         self.assertEqual(set(pi1.keys()), set(pi2.keys()))
 
         for k in pi1.keys():
-            self.assertEqual(pi1[k], pi2[k], k)
+            d1, d2 = pi1[k], pi2[k]
+            self.assertEqual(d1.keys(), d2.keys(), k)
+            for k2 in d1.keys():
+                prob1, tags1 = d1[k2]
+                prob2, tags2 = d2[k2]
+                self.assertAlmostEqual(prob1, prob2)
+                self.assertEqual(tags1, tags2)
